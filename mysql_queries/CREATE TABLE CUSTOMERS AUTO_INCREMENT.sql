12d1b137e31535dbf0e9b1c69579bdbbd4f5ef21
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-11-28 17:02:16 +0530

    First commit...

---
+++ mysql_queries/CREATE TABLE CUSTOMERS AUTO_INCREMENT.sql
@@ -0,0 +1,17 @@
+CREATE TABLE CUSTOMERS (
+   ID INT AUTO_INCREMENT,
+   NAME VARCHAR (20) NOT NULL,
+   AGE INT NOT NULL,
+   ADDRESS CHAR (25),
+   SALARY DECIMAL (18, 2),
+   PRIMARY KEY (ID)
+);
+
+INSERT INTO CUSTOMERS VALUES 
+(NULL, 'Ramesh', 32, 'Ahmedabad', 2000.00),
+(NULL, 'Khilan', 25, 'Delhi', 1500.00),
+(NULL, 'Kaushik', 23, 'Kota', 2000.00),
+(NULL, 'Chaitali', 25, 'Mumbai', 6500.00),
+(NULL, 'Hardik', 27, 'Bhopal', 8500.00),
+(NULL, 'Komal', 22, 'Hyderabad', 4500.00),
+(NULL, 'Muffy', 24, 'Indore', 10000.00);
