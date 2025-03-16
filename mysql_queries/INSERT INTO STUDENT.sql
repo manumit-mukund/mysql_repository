397e46157396207787b26698aeed7c17936816c2
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-03-16 19:58:44 +0530

    Commit changes...

---
+++ mysql_queries/INSERT INTO STUDENT.sql
@@ -0,0 +1,24 @@
+SELECT 
+    *
+FROM
+    myhibernate.student;
+
+INSERT INTO STUDENT VALUES(1, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(2, 'C2', '2 months', 'Delhi', 'S2');
+
+INSERT INTO STUDENT VALUES(3, 'C3', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(4, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(5, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(6, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(7, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(8, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(9, 'C1', '2 months', 'Delhi', 'S1');
+
+INSERT INTO STUDENT VALUES(10, 'C1', '2 months', 'Delhi', 'S1');
