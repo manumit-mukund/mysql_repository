def11b4de1ba82828b5b72a6c70eabbcc5d6e7be
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-03-16 20:00:18 +0530

    Commit changes...

---
+++ mysql_queries/INSERT INTO person.sql
@@ -0,0 +1,30 @@
+INSERT INTO person
+(`personID`,
+`name`,
+`address`,
+`phone`,
+`email`,
+`year`)
+VALUES
+(1,
+"N1",
+"A1",
+"123",
+"abc@yahoo.com",
+2024);
+
+INSERT INTO person
+(`personID`,
+`name`,
+`address`,
+`phone`,
+`email`,
+`year`)
+VALUES
+(2,
+"N2",
+"A2",
+"124",
+"abc2@yahoo.com",
+2024);
+
