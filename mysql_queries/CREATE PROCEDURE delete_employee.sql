13b331d61b004b9e96aff6aa19e4bbb17aa438f4
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-07-22 17:09:43 +0530

    Commit a new file...

---
+++ mysql_queries/CREATE PROCEDURE delete_employee.sql
@@ -0,0 +1,10 @@
+DROP PROCEDURE IF EXISTS delete_employee; 
+
+DELIMITER //
+
+CREATE PROCEDURE delete_employee(IN E_ID BIGINT)
+BEGIN
+    DELETE FROM Employee WHERE ID = E_ID;
+END //
+
+DELIMITER ;
